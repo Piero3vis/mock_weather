8cdd15e0569b403822e8912a9432af7eb3348472
Piero3vis <109952035+Piero3vis@users.noreply.github.com>
2024-12-03 22:55:59 +0100

    Create __init__.py

---
+++ src/weather_insights/__init__.py
@@ -0,0 +1,5 @@
+from .data_fetcher import fetch_weather_data
+from .analyzer import WeatherAnalyzer
+from .visualizer import WeatherVisualizer
+
+__all__ = ["fetch_weather_data", "WeatherAnalyzer", "WeatherVisualizer"]
